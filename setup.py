e003aee9f777b0ec187ad32539dd808982e6b414
bha <onbings@gmail.com>
2023-12-19 16:40:07 +0100

    a

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+
 setup(
     name='BofPy',
-    packages=find_packages(),
     version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+        'tkhtmlview>=0.2.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'BofPy=BofPy.Bof_Init:main',  # Optional: If you have command-line scripts
+        ],
+    },
+    author='onbings',
+    author_email='onbings@gmail.com',
     description='BofPy is a python fork of BofStd C++ library',
-    author='Bha',
-    author_email='b.harmel@gmail.com',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/onbings/bofpy',
     license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+    ],
 )
